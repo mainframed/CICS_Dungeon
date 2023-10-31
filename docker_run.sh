6463410a9d8986a50b87b4823e403bffcbf87d60
mainframed <mainframed767@gmail.com>
2023-10-30 20:15:27 -0700

    making docker run better

--- docker_run.sh
+++ docker_run.sh
@@ -8,12 +8,12 @@ docker run -d \
       	-p 3505:3505 \
       	-p 3506:3506 \
       	-p 8888:8888 \
-        -v /home/defcon31/DEV/CICSDGN/kicks:/config \
-        -v /home/defcon31/DEV/CICSDGN/kicks/printers:/printers \
-        -v /home/defcon31/DEV/CICSDGN/kicks/punchcards:/punchcards \
-        -v /home/defcon31/DEV/CICSDGN/kicks/logs:/logs \
-        -v /home/defcon31/DEV/CICSDGN/kicks/dasd:/dasd \
-        -v /home/defcon31/DEV/CICSDGN/kicks/certs:/certs \
+        -v $(pwd)/kicks:/config \
+        -v $(pwd)/kicks/printers:/printers \
+        -v $(pwd)/kicks/punchcards:/punchcards \
+        -v $(pwd)/kicks/logs:/logs \
+        -v $(pwd)/kicks/dasd:/dasd \
+        -v $(pwd)/kicks/certs:/certs \
       	--restart unless-stopped \
-      	mainframed767/kicks:1.5.0
+      	mainframed767/kicks:1.5.1
 
